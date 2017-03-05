c724bcc72694a04b7797384d62a7297fef71ee14
Gavin Mak <gavinmak98@gmail.com>
2017-03-05 18:50:17 +0000

    fix server

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ def index():
 @app.route("/result", methods=["POST"])
 def result():
     # Imports the Google Cloud client library
-    from google.cloud import language
+    from google-cloud-python-master import language
     import numpy, re
     from scipy import stats
 
